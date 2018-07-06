f36f13c36adb90fa254b2b0a2c4271e4c4c84eee
Kamila <kmuborakova@gmail.com>
2018-07-06 10:45:24 +0600

    FactoryTests

--- WindowsFormsApp1/WindowsFormsApp1.Tests/TwoArgument.Test/DivideTest.cs
+++ WindowsFormsApp1/WindowsFormsApp1.Tests/TwoArgument.Test/DivideTest.cs
@@ -4,21 +4,21 @@ namespace WindowsFormsApp1.Tests.TwoArgument.Test
 
 {
     class DivideTest
-    { 
+    {
         [TestFixture]
         public class TestClass
-    {
-        [TestCase(12, 3, 4)]
-        [TestCase(8, 4, 2)]
-        [TestCase(10, 2, 5)]
-        public void CalculateTest(double firstValue, double secondValue, double expected)
         {
-            var calculator = new Divide();
-            var actualResult = calculator.Calculate(firstValue, secondValue);
-            Assert.AreEqual(expected, actualResult);
-        }
+            [TestCase(12, 3, 4)]
+            [TestCase(8, 4, 2)]
+            [TestCase(10, 2, 5)]
+            public void CalculateTest(double firstValue, double secondValue, double expected)
+            {
+                var calculator = new Divide();
+                var actualResult = calculator.Calculate(firstValue, secondValue);
+                Assert.AreEqual(expected, actualResult);
+            }
         }
     }
-   
-        }
+
+}
     
--- WindowsFormsApp1/WindowsFormsApp1.Tests/TwoArgument.Test/MultiplyTest.cs
+++ WindowsFormsApp1/WindowsFormsApp1.Tests/TwoArgument.Test/MultiplyTest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WindowsFormsApp1.TwoArguments;
+﻿using WindowsFormsApp1.TwoArguments;
 using NUnit.Framework;
 
 namespace WindowsFormsApp1.Tests.TwoArgument.Test
